00ac37eadcde8fdff7d4214828ddc4f28c729e75
neetu-kushwaha1227 <neetukushwaha319@gmail.com>
2025-08-14 23:55:32 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,27 +5,21 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-	/*public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Connection conn=null;
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		 conn= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","Neetuk123");
-		 System.out.println("✅ Database connected successfully!");
-
-
-	}*/
+	   private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+       private static final String USER = "root";
+       private static final String PASSWORD = "your_mysql_password";
+	
 	   public static Connection getConnection() throws Exception{
 	        Connection conn = null;
 	        try {
 	            Class.forName("oracle.jdbc.driver.OracleDriver"); // Oracle driver
-	            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","Neetuk123");
-	           // System.out.println("✅ Database connected successfully!");
+	            conn = DriverManager.getConnection("URL","USER","PASSWORD");
 	            conn.setAutoCommit(true); // auto-commit ON for Oracle
 	        } catch (SQLException e) {
-	            System.out.println("❌ Database connection failed!");
 	            e.printStackTrace();
 	        }
 	        return conn;
 	    }
 
 }
+
